fb65564eb856b3db69556a5ce0e147df5bc1fff8
Evan-Maddicks <evan.maddicks@mths.ca>
2024-10-23 10:53:12 -0400

    Servo now moves to the 45 and 135 degree angles

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Oct 2024
+ * This program will change a servo's angle.
 */
 
-basic.showString('Hello, World!')
+//variable
+let servo1 = robotbit.Servos.S1
+
+//cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//setup
+input.onButtonPressed(Button.A,function() {
+    robotbit.Servo(servo1,45)
+    basic.clearScreen()
+    basic.showNumber(45)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servo1, 135)
+    basic.clearScreen()
+    basic.showNumber(135)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
